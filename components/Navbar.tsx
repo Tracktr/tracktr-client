d084f8550129bfda121769f06bbacb816d4c2f92
Patrick Roelofs <contact@patrickroelofs.com>
2022-09-30 11:34:58 +0200

    change padding on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const navLinks = [
 
 const Navbar = () => (
   <nav className="fixed top-0 w-screen bg-opacity-25 bg-primaryBackground filter backdrop-blur-md">
-    <div className="max-w-6xl px-6 py-4 m-auto">
+    <div className="max-w-6xl px-6 py-2 m-auto">
       <div className="flex justify-between">
         <Link href="/">
           <a>
@@ -33,12 +33,10 @@ const Navbar = () => (
               <HiSearch />
             </button> */}
           <NavButton href="/signin" text="Sign in" active={false} />
-          {/* 
-          // TODO: User profile in navbar
           <div className="flex items-center text-white">
             <Image src="https://picsum.photos/100" width="36px" height="36px" className="rounded-full" />
             <p className="ml-2">Username</p>
-          </div> */}
+          </div>
         </div>
       </div>
     </div>
