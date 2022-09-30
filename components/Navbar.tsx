769983edd86a8d9e02f4ef9b59c6b9e67440396c
Patrick Roelofs <contact@patrickroelofs.com>
2022-09-30 10:28:49 +0200

    Navbar styling, eslint changes, react-icons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,12 @@
+import { HiSearch } from 'react-icons/hi';
+import NavButton from './Navbutton';
+
+const navLinks = [
+  { href: '#', text: 'Movies', active: false },
+  { href: '#', text: ' Series', active: false },
+  { href: '#', text: ' Calendar', active: false },
+];
+
 const Navbar = () => {
   return (
     <nav className="fixed top-0 w-screen bg-opacity-25 bg-primaryBackground filter backdrop-blur-md">
@@ -7,6 +16,24 @@ const Navbar = () => {
             TRACKTR
             <span className="text-primary">.</span>
           </h1>
+          <ul className="flex justify-between">
+            {navLinks.map((navItem) => {
+              return (
+                <NavButton
+                  key={navItem.text}
+                  href={navItem.href}
+                  text={navItem.text}
+                  active={navItem.active}
+                />
+              );
+            })}
+          </ul>
+          <div className="flex items-center justify-between">
+            <button type="button" className="mr-4 text-base text-white">
+              <HiSearch />
+            </button>
+            <NavButton href="#" text="Sign in" active={false} />
+          </div>
         </div>
       </div>
     </nav>
