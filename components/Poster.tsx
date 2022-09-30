894a08e57933e7a01c16cefa000da45e5f2e979d
Rowan-Paul <contact@rowanpaulflynn.com>
2022-09-30 10:52:14 +0200

    Lint

--- components/Poster.tsx
+++ components/Poster.tsx
@@ -1,17 +1,15 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export interface IPoster {
   imageSrc: string;
   name: string;
 }
 
-const Poster = ({ imageSrc, name }: IPoster) => {
-  return (
-    <div>
-      <Image src={imageSrc} width="170px" height="240px" className="rounded" />
-      <div className="text-xs">{name}</div>
-    </div>
-  );
-};
+const Poster = ({ imageSrc, name }: IPoster) => (
+  <div>
+    <Image src={imageSrc} width="170px" height="240px" className="rounded" />
+    <div className="text-xs">{name}</div>
+  </div>
+);
 
 export default Poster;
