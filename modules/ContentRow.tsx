a18d28d5e4e1001dde2460c7c6664ce83a70a5b6
Rowan-Paul <contact@rowanpaulflynn.com>
2022-09-30 10:57:59 +0200

    Add key to poster list

--- modules/ContentRow.tsx
+++ modules/ContentRow.tsx
@@ -1,25 +1,23 @@
-import Poster, { IPoster } from '../components/Poster';
-import SortPill from '../components/SortPill';
+import Poster, { IPoster } from "../components/Poster";
+import SortPill from "../components/SortPill";
 
 interface IContentRow {
   type: string;
   data: IPoster[];
 }
 
-const ContentRow = ({ type, data }: IContentRow) => {
-  return (
-    <div>
-      <div className="flex">
-        <div className="text-4xl mb-6">{type}</div>
-        <SortPill />
-      </div>
-      <div className="flex gap-6 justify-center">
-        {data.map((p) => {
-          return <Poster imageSrc={p.imageSrc} name={p.name} />;
-        })}
-      </div>
+const ContentRow = ({ type, data }: IContentRow) => (
+  <div>
+    <div className="flex">
+      <div className="text-4xl mb-6">{type}</div>
+      <SortPill />
     </div>
-  );
-};
+    <div className="flex gap-6 justify-center">
+      {data.map((p) => (
+        <Poster key={p.name} imageSrc={p.imageSrc} name={p.name} />
+      ))}
+    </div>
+  </div>
+);
 
 export default ContentRow;
