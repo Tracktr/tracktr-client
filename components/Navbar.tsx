18225ce928097e302a36638de991987c8ac98268
Patrick Roelofs <contact@patrickroelofs.com>
2022-10-02 18:07:08 +0200

    add mobile buttons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { useState } from "react";
+import { CgMenuGridO } from "react-icons/cg";
 import NavButton from "./Navbutton";
 import NavSearch from "./Navsearch";
 
@@ -8,30 +10,52 @@ const navLinks = [
   { href: "/calendar", text: " Calendar", active: false },
 ];
 
-const Navbar = () => (
-  <nav className="fixed top-0 w-screen bg-opacity-25 bg-primaryBackground filter backdrop-blur-md">
-    <div className="max-w-6xl px-6 py-2 m-auto">
-      <div className="grid grid-cols-3">
-        <Link href="/">
-          <a className="col-span-1">
-            <h1 className="text-3xl font-black text-white">
-              TRACKTR
-              <span className="text-primary">.</span>
-            </h1>
-          </a>
-        </Link>
-        <ul className="flex items-center justify-center">
-          {navLinks.map((navItem) => (
-            <NavButton key={navItem.text} href={navItem.href} text={navItem.text} active={navItem.active} />
-          ))}
-        </ul>
-        <div className="flex items-center justify-end col-span-1">
-          <NavSearch />
-          <NavButton href="/signin" text="Sign in" active={false} />
+const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
+  const openMenu = () => {
+    setMenu(!menu);
+  };
+
+  return (
+    <nav className="fixed top-0 w-screen bg-opacity-25 bg-primaryBackground filter backdrop-blur-md">
+      <div className="max-w-6xl px-6 py-2 m-auto">
+        <div className="grid grid-cols-3">
+          <Link href="/">
+            <a className="col-span-1">
+              <h1 className="text-3xl font-black text-white">
+                TRACKTR
+                <span className="text-primary">.</span>
+              </h1>
+            </a>
+          </Link>
+          <ul className="items-center justify-center hidden lg:flex">
+            {navLinks.map((navItem) => (
+              <NavButton key={navItem.text} href={navItem.href} text={navItem.text} active={navItem.active} />
+            ))}
+          </ul>
+          <div className="flex items-center justify-end col-span-2 lg:col-span-1">
+            <button type="button" className="block cursor-pointer lg:hidden" onClick={openMenu}>
+              <CgMenuGridO className="text-2xl" />
+            </button>
+            <div className="hidden lg:block">
+              <NavSearch />
+            </div>
+            <NavButton href="/signin" text="Sign in" active={false} />
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
-);
+      {menu && (
+        <div className="block w-full lg:hidden">
+          <ul className="flex items-center justify-center align-middle">
+            {navLinks.map((navItem) => (
+              <NavButton key={navItem.text} href={navItem.href} text={navItem.text} active={navItem.active} />
+            ))}
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+};
 
 export default Navbar;
