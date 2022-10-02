e175694be51f442ae5cf24f7818fb6bb69041d64
Patrick Roelofs <contact@patrickroelofs.com>
2022-10-02 18:27:27 +0200

    cleanup, remove navsearch as unnecessary rn

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import { useState } from "react";
 import { CgMenuGridO } from "react-icons/cg";
 import NavButton from "./Navbutton";
-import NavSearch from "./Navsearch";
 
 const navLinks = [
   { href: "/movies", text: "Movies", active: false },
@@ -18,7 +17,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="fixed top-0 w-screen bg-opacity-25 bg-primaryBackground filter backdrop-blur-md">
+    <nav className="fixed top-0 z-50 w-screen bg-opacity-25 bg-primaryBackground filter backdrop-blur-md">
       <div className="max-w-6xl px-6 py-2 m-auto">
         <div className="grid grid-cols-3">
           <Link href="/">
@@ -38,9 +37,6 @@ const Navbar = () => {
             <button type="button" className="block cursor-pointer lg:hidden" onClick={openMenu}>
               <CgMenuGridO className="text-2xl" />
             </button>
-            <div className="hidden lg:block">
-              <NavSearch />
-            </div>
             <NavButton href="/signin" text="Sign in" active={false} />
           </div>
         </div>
