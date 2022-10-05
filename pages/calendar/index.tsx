0a8ab26f0382c37ebc7c6b95d31b9a2ceeb23467
Patrick Roelofs <contact@patrickroelofs.com>
2022-10-05 14:53:31 +0200

    delete unused files

--- pages/calendar/index.tsx
+++
@@ -1,5 +0,0 @@
-import { NextPage } from "next/types";
-
-const Calendar: NextPage = () => <div />;
-
-export default Calendar;
--- pages/profile/index.tsx
+++
@@ -1,5 +0,0 @@
-import { NextPage } from "next/types";
-
-const Profile: NextPage = () => <div />;
-
-export default Profile;
