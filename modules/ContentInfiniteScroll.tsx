be31f2d86d0330371b10a2037472bfbae943b1ee
Rowan-Paul <contact@rowanpaulflynn.com>
2022-10-06 10:49:00 +0200

    Add content title

--- modules/ContentInfiniteScroll.tsx
+++ modules/ContentInfiniteScroll.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useRef } from "react";
 import { useInfiniteQuery } from "react-query";
+import { LoadingPoster } from "../components/LoadingPosters";
 import Poster from "../components/Poster";
 
 interface IContentInfiniteScroll {
@@ -41,25 +42,23 @@ const ContentInfiniteScroll = ({ fetchContent, type }: IContentInfiniteScroll) =
   }, [fetchNextPage, hasNextPage, handleObserver]);
 
   return (
-    <div className="flex flex-wrap items-center gap-4 py-5">
-      {isSuccess &&
-        data.pages?.map((page) =>
-          page.results.map((content: any) => (
-            <Poster
-              imageSrc={`https://image.tmdb.org/t/p/w185${content.poster_path}`}
-              name={content.title || content.name}
-              id={content.id}
-              type={type}
-            />
-          ))
-        )}
-      <div className="loader" ref={observerElem}>
-        {isFetchingNextPage && hasNextPage && (
-          <div className="flex flex-col gap-y-[6px]">
-            <div className="animate-pulse w-[170px] h-[240px] rounded bg-[#343434]" />
-            <div className="animate-pulse h-4 w-[170px] rounded bg-[#343434]" />
-          </div>
-        )}
+    <div>
+      <div className="z-40 text-4xl">{type}</div>
+      <div className="flex flex-wrap items-center gap-4 py-5">
+        {isSuccess &&
+          data.pages?.map((page) =>
+            page.results.map((content: any) => (
+              <Poster
+                imageSrc={`https://image.tmdb.org/t/p/w185${content.poster_path}`}
+                name={content.title || content.name}
+                id={content.id}
+                type={type}
+              />
+            ))
+          )}
+        <div className="loader" ref={observerElem}>
+          {isFetchingNextPage && hasNextPage && <LoadingPoster />}
+        </div>
       </div>
     </div>
   );
