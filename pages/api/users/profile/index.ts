e5d7e9f5d823fd9c55d71c4ff2666c680e2d9ae0
Patrick Roelofs <contact@patrickroelofs.com>
2022-10-17 21:38:03 +0200

    add toggle adult option and change in database

--- pages/api/users/profile/index.ts
+++ pages/api/users/profile/index.ts
@@ -9,12 +9,40 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   if (!session) return res.status(401).json({ unauthorized: true });
 
-  const user = await prisma.user.findFirst<Prisma.UserFindFirstArgs>({
-    where: { email: session.user?.email },
-  });
+  if (req.method === "GET") {
+    const user = await prisma.user.findFirst<Prisma.UserFindFirstArgs>({
+      where: { email: session.user?.email },
+      include: {
+        profile: true,
+      },
+    });
 
-  return res.status(200).json({
-    user,
-    session,
-  });
+    return res.status(200).json({
+      user,
+    });
+  }
+
+  if (req.method === "POST") {
+    const user = await prisma.user.findFirst<Prisma.UserFindFirstArgs>({
+      where: { email: session.user?.email },
+    });
+
+    const editProfile = await prisma.user.update({
+      where: {
+        id: user?.id,
+      },
+      data: {
+        profile: {
+          update: {
+            // eslint-disable-next-line no-unneeded-ternary
+            adult: req.body.adult.toLowerCase() === "true" ? true : false,
+          },
+        },
+      },
+    });
+
+    return res.status(200).json(editProfile);
+  }
+
+  return res.status(301);
 }
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,8 +1,57 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import LoadingPageComponents from "@/components/common/loading/LoadingPageComponents";
+import ProfileHeader from "@/components/profile/ProfileHeader";
 import { getSession } from "next-auth/react";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
-const ProfilePage = ({ session }: any) => {
-  console.log(session);
-  return <p className="pt-64 text-6xl text-center">{session.user.name}</p>;
+const fetchProfile = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/users/profile`);
+
+  return res.json();
+};
+
+const ProfilePage = () => {
+  const queryClient = useQueryClient();
+  const { data, status } = useQuery(["User"], fetchProfile);
+  const mutation = useMutation(
+    (formData: any) =>
+      fetch(`${process.env.NEXT_PUBLIC_URL}/api/users/profile`, {
+        method: "POST",
+        body: new URLSearchParams(formData),
+      }),
+    {
+      onSuccess: () => queryClient.invalidateQueries(["User"]),
+    }
+  );
+
+  const onSubmit = (event: any) => {
+    event.preventDefault();
+
+    mutation.mutate(new FormData(event.target));
+  };
+
+  return (
+    <LoadingPageComponents status={status}>
+      {() => (
+        <div>
+          <ProfileHeader image={data.user.image} name={data.user.name} />
+          <div className="max-w-6xl pt-16 m-auto">
+            <form className="flex flex-col" onSubmit={onSubmit}>
+              <div>
+                <label htmlFor="adult">Adult:</label>
+                <select name="adult" className="text-black">
+                  <option value="true">Show adult 18+ content</option>
+                  <option value="false">Hide adult 18+ content</option>
+                </select>
+              </div>
+
+              <input type="submit" />
+            </form>
+          </div>
+        </div>
+      )}
+    </LoadingPageComponents>
+  );
 };
 
 export async function getServerSideProps(context: any) {
