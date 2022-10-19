1a73d5bfa9bc668fd2c6a4c2933e16aa9f3f44a6
Patrick Roelofs <contact@patrickroelofs.com>
2022-10-19 14:24:51 +0200

    kinda weird

--- pages/api/users/profile/index.ts
+++ pages/api/users/profile/index.ts
@@ -1,11 +1,12 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from "next-auth/react";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "pages/api/auth/[...nextauth]";
 
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const session = await getSession({ req });
+  const session = await unstable_getServerSession(req, res, authOptions);
 
   if (!session) return res.status(401).json({ unauthorized: true });
 
