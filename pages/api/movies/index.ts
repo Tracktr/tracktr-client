73affbde869d26f3e44c2f25850deca587a19893
Patrick Roelofs <contact@patrickroelofs.com>
2022-10-19 12:43:24 +0200

    apply session to movie/tv call

--- pages/api/movies/index.ts
+++ pages/api/movies/index.ts
@@ -1,4 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Session } from "next-auth";
+import { getSession } from "next-auth/react";
 
 interface Props {
   type: string | string[] | undefined;
@@ -6,13 +8,15 @@ interface Props {
   sortby?: string | string[] | undefined;
   page?: string | string[] | undefined;
   filter?: string | string[] | undefined;
+  session: Session | null;
 }
 
-async function retrieveMoviesData({ type, limiter, sortby, page, filter }: Props) {
+async function retrieveMoviesData({ type, limiter, sortby, page, filter, session }: Props) {
   const url = new URL(`${type}/${limiter}${filter ? `/${filter}` : ""}`, process.env.NEXT_PUBLIC_TMDB_API);
   url.searchParams.append("api_key", process.env.NEXT_PUBLIC_TMDB_KEY || "");
   if (sortby) url.searchParams.append("sort_by", sortby.toString());
   if (page) url.searchParams.append("page", page.toString());
+  if (session) url.searchParams.append("language", session.user.profile.language);
 
   const res = await fetch(url);
   const json = await res.json();
@@ -21,9 +25,10 @@ async function retrieveMoviesData({ type, limiter, sortby, page, filter }: Props
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getSession({ req });
   const { type, limiter, sortby, page, filter } = req.query;
 
-  const moviesData = await retrieveMoviesData({ type, limiter, sortby, page, filter });
+  const moviesData = await retrieveMoviesData({ type, limiter, sortby, page, filter, session });
 
   res.status(200).json({
     ...moviesData,
