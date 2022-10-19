bc993c50c94a31f7688d1d867a9ed0d3c829bf9d
Patrick Roelofs <contact@patrickroelofs.com>
2022-10-19 20:16:08 +0200

    literally yeet everything and implement TRPC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,42 +1,16 @@
-import type { DehydratedState } from "react-query";
-import type { AppProps } from "next/app";
-
+// src/pages/_app.tsx
 import "../styles/globals.css";
-import "../styles/nprogress.css";
-
-import { useState } from "react";
-import { QueryClient, QueryClientProvider, Hydrate } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import { SessionProvider } from "next-auth/react";
-import { Session } from "next-auth";
-import Router from "next/router";
-import NProgress from "nprogress";
+import type { Session } from "next-auth";
+import type { AppType } from "next/app";
+import { trpc } from "../utils/trpc";
 
-import Layout from "src/components/common/Layout";
-import Head from "next/head";
-
-NProgress.configure({ showSpinner: false });
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeError", () => NProgress.done());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-
-function MyApp({ Component, pageProps }: AppProps<{ dehydratedState: DehydratedState; session: Session }>) {
-  const [queryClient] = useState(() => new QueryClient());
+const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <SessionProvider session={pageProps.session}>
-          <Layout>
-            <Head>
-              <meta name="theme-color" content="#101010" />
-            </Head>
-            <Component {...pageProps} />
-          </Layout>
-          <ReactQueryDevtools initialIsOpen={false} />
-        </SessionProvider>
-      </Hydrate>
-    </QueryClientProvider>
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
-}
+};
 
-export default MyApp;
+export default trpc.withTRPC(MyApp);
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,14 +1,12 @@
-import { Profile } from "@prisma/client";
-import NextAuth, { DefaultSession } from "next-auth";
+import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    user: {
+    user?: {
       id: string;
-      profile: Profile;
     } & DefaultSession["user"];
   }
 }
