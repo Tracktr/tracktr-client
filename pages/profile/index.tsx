48c1127883d133bdd3aaa686ccd4edafd4ab468a
Patrick Roelofs <contact@patrickroelofs.com>
2022-10-19 14:01:45 +0200

    testing deployment error part 2

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -98,10 +98,10 @@ const ProfilePage = (props: { languages: any }) => {
 
 export async function getServerSideProps(context: any) {
   const session = await getSession(context);
-  console.log(session);
   const languagesReq = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/languages`);
   const languagesRes = await languagesReq.json();
-  console.log(languagesRes);
+  console.log(session ? "there is a session" : "there is no session");
+  console.log(session);
 
   if (!session) {
     return {
