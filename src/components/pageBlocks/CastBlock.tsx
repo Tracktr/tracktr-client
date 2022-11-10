c7d03945c5e26ce16d44eaa822de36821856ca04
Rowan-Paul <contact@rowanpaulflynn.com>
2022-11-10 14:04:37 +0100

    Fix glitching castblock on firefox mobile

--- src/components/pageBlocks/CastBlock.tsx
+++ src/components/pageBlocks/CastBlock.tsx
@@ -8,7 +8,7 @@ interface ICast {
 const CastBlock = ({ cast }: ICast) => {
   if (cast.length > 0) {
     return (
-      <div className="relative md:mb-24">
+      <div className="relative mx-1 md:mx-0 md:mb-24">
         <h2 className="pb-4 text-4xl font-bold">Cast</h2>
         <div>
           <HorizontalScrollContainer>
