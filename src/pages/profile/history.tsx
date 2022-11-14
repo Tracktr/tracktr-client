7e91628e9ca2efa329160dd2090b899f3d465a81
Rowan-Paul <contact@rowanpaulflynn.com>
2022-11-14 12:20:36 +0100

    Add poster

--- src/pages/profile/history.tsx
+++ src/pages/profile/history.tsx
@@ -1,8 +1,12 @@
 import { useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import LoadingPageComponents from "../../components/common/LoadingPageComponents";
 import ProfileHeader from "../../components/pageBlocks/ProfileHeader";
+import MoviePoster from "../../components/posters/MoviePoster";
+import { PosterImage } from "../../utils/generateImages";
 import { trpc } from "../../utils/trpc";
 
 const ProfilePage = () => {
@@ -48,14 +52,66 @@ const ProfilePage = () => {
                 );
 
                 return (
-                  <div key={item.id}>
-                    <div className="text-sm truncate">
-                      {item?.series
-                        ? `${item.season_number}x${item.episode_number} ${item.series.name}`
-                        : `${item.movie.title}`}
-                    </div>
-                    <div className="text-xs">{date}</div>
-                  </div>
+                  <Link
+                    href={
+                      item?.movie_id
+                        ? `/movies/${item.movie.id}`
+                        : `/tv/${item.series_id}/season/${item.season_number}/episode/${item.episode_number}`
+                    }
+                    key={item.id}
+                  >
+                    <a>
+                      {/* <div className="relative">
+                        <Image
+                          alt={`Poster image for ${
+                            item?.movie_id
+                              ? item.movie.title
+                              : `${item.season_number}x${item.episode_number} ${item.series.name}`
+                          }`}
+                          src={PosterImage({
+                            path: item.movie_id ? item.movie.poster : item.series.poster,
+                            size: "sm",
+                          })}
+                          width="170px"
+                          height="240px"
+                          className="rounded"
+                        />
+
+                        <div className="absolut">
+                          <span className="text-sm truncate">
+                            {item?.movie_id
+                              ? `${item.movie.title}`
+                              : `${item.season_number}x${item.episode_number} ${item.series.name}`}
+                          </span>
+                          <div className="text-xs">{date}</div>
+                        </div>
+                      </div> */}
+                      <div className="relative w-[170px]">
+                        <Image
+                          alt={`Poster image for ${
+                            item?.movie_id
+                              ? item.movie.title
+                              : `${item.season_number}x${item.episode_number} ${item.series.name}`
+                          }`}
+                          src={PosterImage({
+                            path: item.movie_id ? item.movie.poster : item.series.poster,
+                            size: "sm",
+                          })}
+                          width="170px"
+                          height="240px"
+                          className="rounded"
+                        />
+                        <div className="absolute bottom-0 left-0 right-0 px-4 py-2 bg-black bg-opacity-75">
+                          <span className="text-sm">
+                            {item?.movie_id
+                              ? `${item.movie.title}`
+                              : `${item.season_number}x${item.episode_number} ${item.series.name}`}
+                          </span>
+                          <div className="text-xs">{date}</div>
+                        </div>
+                      </div>
+                    </a>
+                  </Link>
                 );
               })}
           </div>
