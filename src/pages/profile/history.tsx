46c57bdbec526aa5ae2015ae65468dc85f3a368e
Rowan-Paul <contact@rowanpaulflynn.com>
2022-11-14 12:30:10 +0100

    Add fallback

--- src/pages/profile/history.tsx
+++ src/pages/profile/history.tsx
@@ -34,63 +34,67 @@ const ProfilePage = () => {
         <div className="max-w-6xl m-auto">
           <ProfileHeader image={data?.image} name={data?.name} />
           <h1 className="mt-6 text-3xl">History</h1>
-          <div className="grid justify-center gap-2 my-6 text-center md:grid-cols-4 lg:grid-cols-6">
-            {history
-              .sort((a, b) => {
-                if (a.datetime < b.datetime) {
-                  return 1;
-                } else {
-                  return -1;
-                }
-              })
-              .map((item) => {
-                const date = new Date(item.datetime).toLocaleString(
-                  "en-UK", // TODO: get time format from user language
-                  {
-                    dateStyle: "medium",
-                    timeStyle: "short",
+          {history.length > 0 ? (
+            <div className="grid justify-center gap-2 my-6 text-center md:grid-cols-4 lg:grid-cols-6">
+              {history
+                .sort((a, b) => {
+                  if (a.datetime < b.datetime) {
+                    return 1;
+                  } else {
+                    return -1;
                   }
-                );
-
-                return (
-                  <Link
-                    href={
-                      item?.movie_id
-                        ? `/movies/${item.movie.id}`
-                        : `/tv/${item.series_id}/season/${item.season_number}/episode/${item.episode_number}`
+                })
+                .map((item) => {
+                  const date = new Date(item.datetime).toLocaleString(
+                    "en-UK", // TODO: get time format from user language
+                    {
+                      dateStyle: "medium",
+                      timeStyle: "short",
                     }
-                    key={item.id}
-                  >
-                    <a>
-                      <div className="relative w-[170px]">
-                        <Image
-                          alt={`Poster image for ${
-                            item?.movie_id
-                              ? item.movie.title
-                              : `${item.season_number}x${item.episode_number} ${item.series.name}`
-                          }`}
-                          src={PosterImage({
-                            path: item.movie_id ? item.movie.poster : item.series.poster,
-                            size: "sm",
-                          })}
-                          width="170px"
-                          height="240px"
-                          className="rounded"
-                        />
-                        <div className="absolute bottom-0 left-0 right-0 px-4 py-2 bg-black bg-opacity-75">
-                          <span className="w-full text-sm">
-                            {item?.movie_id
-                              ? `${item.movie.title}`
-                              : `${item.season_number}x${item.episode_number} ${item.series.name}`}
-                          </span>
-                          <div className="text-xs">{date}</div>
+                  );
+
+                  return (
+                    <Link
+                      href={
+                        item?.movie_id
+                          ? `/movies/${item.movie.id}`
+                          : `/tv/${item.series_id}/season/${item.season_number}/episode/${item.episode_number}`
+                      }
+                      key={item.id}
+                    >
+                      <a>
+                        <div className="relative w-[170px]">
+                          <Image
+                            alt={`Poster image for ${
+                              item?.movie_id
+                                ? item.movie.title
+                                : `${item.season_number}x${item.episode_number} ${item.series.name}`
+                            }`}
+                            src={PosterImage({
+                              path: item.movie_id ? item.movie.poster : item.series.poster,
+                              size: "sm",
+                            })}
+                            width="170px"
+                            height="240px"
+                            className="rounded"
+                          />
+                          <div className="absolute bottom-0 left-0 right-0 px-4 py-2 bg-black bg-opacity-75">
+                            <span className="w-full text-sm">
+                              {item?.movie_id
+                                ? `${item.movie.title}`
+                                : `${item.season_number}x${item.episode_number} ${item.series.name}`}
+                            </span>
+                            <div className="text-xs">{date}</div>
+                          </div>
                         </div>
-                      </div>
-                    </a>
-                  </Link>
-                );
-              })}
-          </div>
+                      </a>
+                    </Link>
+                  );
+                })}
+            </div>
+          ) : (
+            <div>No history found, start watching some shows and movies!</div>
+          )}
         </div>
       )}
     </LoadingPageComponents>
