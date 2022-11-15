73b874f041b21baafd869f2a2a39ddae9218098b
Rowan-Paul <contact@rowanpaulflynn.com>
2022-11-15 10:45:22 +0100

    Update extract HistoryGrid into seperate component

---
+++ src/components/common/HistoryGrid.tsx
@@ -0,0 +1,74 @@
+import { EpisodesHistory, MoviesHistory } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+import { PosterImage } from "../../utils/generateImages";
+import LoadingPageComponents from "./LoadingPageComponents";
+
+interface IHistoryGrid {
+  history: (MoviesHistory | EpisodesHistory)[];
+  status: "error" | "success" | "loading";
+}
+
+const HistoryGrid = ({ history, status }: IHistoryGrid): JSX.Element => {
+  if (history.length < 1 && status !== "loading") {
+    return <div>No history found, start watching some shows and movies!</div>;
+  }
+
+  return (
+    <LoadingPageComponents status={status} posters>
+      {() => (
+        <div className="grid gap-4 py-5 grid-cols-fluid">
+          {history.map((item: any) => {
+            const date = new Date(item.datetime).toLocaleString(
+              "en-UK", // TODO: get time format from user language
+              {
+                dateStyle: "medium",
+                timeStyle: "short",
+              }
+            );
+
+            return (
+              <Link
+                href={
+                  item?.movie_id
+                    ? `/movies/${item.movie.id}`
+                    : `/tv/${item.series_id}/season/${item.season_number}/episode/${item.episode_number}`
+                }
+                key={item.id}
+              >
+                <a className="w-[170px]">
+                  <div className="relative">
+                    <Image
+                      alt={`Poster image for ${
+                        item?.movie_id
+                          ? item.movie.title
+                          : `${item.season_number}x${item.episode_number} ${item.series.name}`
+                      }`}
+                      src={PosterImage({
+                        path: item.movie_id ? item.movie.poster : item.series.poster,
+                        size: "sm",
+                      })}
+                      width="170px"
+                      height="240px"
+                      className="rounded"
+                    />
+                    <div className="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gradient-to-t from-primaryBackground">
+                      <span className="w-full text-sm line-clamp-2">
+                        {item?.movie_id
+                          ? `${item.movie.title}`
+                          : `${item.season_number}x${item.episode_number} ${item.series.name}`}
+                      </span>
+                      <div className="text-xs line-clamp-1">{date}</div>
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            );
+          })}
+        </div>
+      )}
+    </LoadingPageComponents>
+  );
+};
+
+export default HistoryGrid;
--- src/pages/profile/history.tsx
+++ src/pages/profile/history.tsx
@@ -1,80 +1,10 @@
-import { EpisodesHistory, MoviesHistory } from "@prisma/client";
 import { useSession } from "next-auth/react";
-import Image from "next/image";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import HistoryGrid from "../../components/common/HistoryGrid";
 import LoadingPageComponents from "../../components/common/LoadingPageComponents";
 import ProfileHeader from "../../components/pageBlocks/ProfileHeader";
-import { PosterImage } from "../../utils/generateImages";
 import { trpc } from "../../utils/trpc";
-
-interface IHistoryGrid {
-  history: (MoviesHistory | EpisodesHistory)[];
-  status: "error" | "success" | "loading";
-}
-
-const HistoryGrid = ({ history, status }: IHistoryGrid): JSX.Element => {
-  if (history.length < 1 && status !== "loading") {
-    return <div>No history found, start watching some shows and movies!</div>;
-  }
-
-  return (
-    <LoadingPageComponents status={status} posters>
-      {() => (
-        <div className="grid gap-4 py-5 grid-cols-fluid">
-          {history.map((item: any) => {
-            const date = new Date(item.datetime).toLocaleString(
-              "en-UK", // TODO: get time format from user language
-              {
-                dateStyle: "medium",
-                timeStyle: "short",
-              }
-            );
-
-            return (
-              <Link
-                href={
-                  item?.movie_id
-                    ? `/movies/${item.movie.id}`
-                    : `/tv/${item.series_id}/season/${item.season_number}/episode/${item.episode_number}`
-                }
-                key={item.id}
-              >
-                <a className="w-[170px]">
-                  <div className="relative">
-                    <Image
-                      alt={`Poster image for ${
-                        item?.movie_id
-                          ? item.movie.title
-                          : `${item.season_number}x${item.episode_number} ${item.series.name}`
-                      }`}
-                      src={PosterImage({
-                        path: item.movie_id ? item.movie.poster : item.series.poster,
-                        size: "sm",
-                      })}
-                      width="170px"
-                      height="240px"
-                      className="rounded"
-                    />
-                    <div className="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gradient-to-t from-primaryBackground">
-                      <span className="w-full text-sm line-clamp-2">
-                        {item?.movie_id
-                          ? `${item.movie.title}`
-                          : `${item.season_number}x${item.episode_number} ${item.series.name}`}
-                      </span>
-                      <div className="text-xs line-clamp-1">{date}</div>
-                    </div>
-                  </div>
-                </a>
-              </Link>
-            );
-          })}
-        </div>
-      )}
-    </LoadingPageComponents>
-  );
-};
 
 const HistoryPage = () => {
   const router = useRouter();
