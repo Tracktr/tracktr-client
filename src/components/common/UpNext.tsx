77da027cc8570d8ecc48e1941ba09be9ff889835
Rowan-Paul <contact@rowanpaulflynn.com>
2022-11-17 13:55:27 +0100

    Add mark as watched on up next

--- src/components/common/UpNext.tsx
+++ src/components/common/UpNext.tsx
@@ -4,13 +4,16 @@ import { PosterImage } from "../../utils/generateImages";
 import LoadingPageComponents from "./LoadingPageComponents";
 import { PosterGrid } from "./PosterGrid";
 import { Episodes } from "@prisma/client";
+import { ImCheckmark2 } from "react-icons/im";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface IepisodesGrid {
   episodes: Episodes[];
   status: "error" | "success" | "loading";
+  markAsWatched: (e: any) => void;
 }
 
-const UpNext = ({ episodes, status }: IepisodesGrid): JSX.Element => {
+const UpNext = ({ episodes, status, markAsWatched }: IepisodesGrid): JSX.Element => {
   if (episodes.length < 1 && status !== "loading") {
     return (
       <div>
@@ -26,27 +29,59 @@ const UpNext = ({ episodes, status }: IepisodesGrid): JSX.Element => {
     <LoadingPageComponents status={status} posters>
       {() => (
         <PosterGrid hasScrollContainer>
-          {episodes.map((item: any) => {
-            return (
-              <div className="relative w-[170px] group" key={item.id}>
-                <Link href={`/tv/${item.series.id}/season/${item.season_number}/episode/${item.episode_number}`}>
-                  <a className="relative w-[170px] group">
-                    <Image
-                      alt={`Poster image for ${item.season_number}x${item.episode_number} ${item.series.name}`}
-                      src={PosterImage({
-                        path: item.series.poster,
-                        size: "sm",
-                      })}
-                      width="170px"
-                      height="240px"
-                      className="rounded"
-                    />
-                    <div className="absolute bottom-0 left-0 right-0 overflow-hidden text-sm font-bold text-center bg-gradient-to-t from-primaryBackground">{`S${item.season_number} - E${item.episode_number}`}</div>
-                  </a>
-                </Link>
-              </div>
-            );
-          })}
+          <AnimatePresence mode="popLayout" initial={false}>
+            {episodes.map((item: any) => {
+              return (
+                <motion.div
+                  className="relative w-[170px] group"
+                  key={item.id}
+                  layout
+                  initial={{ opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  exit={{ y: -150, opacity: 0 }}
+                  transition={{ type: "spring" }}
+                >
+                  <Link href={`/tv/${item.series.id}/season/${item.season_number}/episode/${item.episode_number}`}>
+                    <a>
+                      <Image
+                        alt={`Poster image for ${`S${item.season_number} - E${item.episode_number}`}`}
+                        src={PosterImage({
+                          path: item.series.poster,
+                          size: "sm",
+                        })}
+                        width="170px"
+                        height="240px"
+                        className="rounded"
+                      />
+                    </a>
+                  </Link>
+                  <div className="absolute bottom-0 left-0 right-0 overflow-hidden bg-gradient-to-t from-primaryBackground">
+                    <Link href={`/tv/${item.series._id}/season/${item.season_number}/episode/${item.episode_number}`}>
+                      <a>
+                        <div className="px-4 py-2">
+                          <span className="w-full text-sm text-center line-clamp-2">
+                            {`S${item.season_number} - E${item.episode_number}`}
+                          </span>
+                        </div>
+                      </a>
+                    </Link>
+                    <button
+                      className="flex justify-center w-full text-3xl text-opacity-100 max-h-0 group-hover:max-h-10 animated"
+                      onClick={() =>
+                        markAsWatched({
+                          episodeNumber: item.episode_number,
+                          seasonNumber: item.season_number,
+                          seriesId: item.series.id,
+                        })
+                      }
+                    >
+                      <ImCheckmark2 />
+                    </button>
+                  </div>
+                </motion.div>
+              );
+            })}
+          </AnimatePresence>
         </PosterGrid>
       )}
     </LoadingPageComponents>
