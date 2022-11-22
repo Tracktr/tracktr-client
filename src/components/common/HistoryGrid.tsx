e26861973f16bbc8ad375ffc13cf1e9271bb3de8
Rowan-Paul <contact@rowanpaulflynn.com>
2022-11-22 11:24:37 +0100

    Fix history loading all episodes

--- src/components/common/HistoryGrid.tsx
+++ src/components/common/HistoryGrid.tsx
@@ -17,6 +17,12 @@ interface IHistoryGrid {
   refetchUpNext: () => void;
 }
 
+interface IEpisode {
+  item: any;
+  refetchHistory: () => void;
+  refetchUpNext: () => void;
+}
+
 const HistoryGrid = ({
   history,
   status,
@@ -24,6 +30,34 @@ const HistoryGrid = ({
   refetchHistory,
   refetchUpNext,
 }: IHistoryGrid): JSX.Element => {
+  return (
+    <LoadingPageComponents status={status} posters>
+      {() => (
+        <PosterGrid hasScrollContainer={hasScrollContainer}>
+          <AnimatePresence mode="popLayout" initial={false}>
+            {history.map((item: any) => {
+              return (
+                <EpisodeHistory
+                  key={item.id}
+                  item={item}
+                  refetchHistory={refetchHistory}
+                  refetchUpNext={refetchUpNext}
+                />
+              );
+            })}
+          </AnimatePresence>
+        </PosterGrid>
+      )}
+    </LoadingPageComponents>
+  );
+};
+
+const EpisodeHistory = ({ item, refetchHistory, refetchUpNext }: IEpisode) => {
+  const date = new Date(item.datetime).toLocaleString("en-UK", {
+    dateStyle: "medium",
+    timeStyle: "short",
+  });
+
   const deleteEpisodeFromHistory = trpc.episode.removeEpisodeFromWatched.useMutation({
     onSuccess: () => {
       refetchHistory();
@@ -46,80 +80,61 @@ const HistoryGrid = ({
   };
 
   return (
-    <LoadingPageComponents status={status} posters>
-      {() => (
-        <PosterGrid hasScrollContainer={hasScrollContainer}>
-          <AnimatePresence mode="popLayout" initial={false}>
-            {history.map((item: any) => {
-              const date = new Date(item.datetime).toLocaleString("en-UK", {
-                dateStyle: "medium",
-                timeStyle: "short",
-              });
-              return (
-                <motion.div
-                  layout
-                  initial={{ opacity: 0 }}
-                  animate={{ y: 0, opacity: 1 }}
-                  exit={{ y: -150, opacity: 0 }}
-                  transition={{ type: "spring" }}
-                  className="relative w-[170px] group"
-                  key={item.id}
-                >
-                  <Link
-                    href={
-                      item?.movie_id
-                        ? `/movies/${item.movie.id}`
-                        : `/tv/${item.series_id}/season/${item.season_number}/episode/${item.episode_number}`
-                    }
-                  >
-                    <a className="relative w-[170px] group">
-                      <Image
-                        alt={`Poster image for ${
-                          item?.movie_id
-                            ? item.movie.title
-                            : `${item.season_number}x${item.episode_number} ${item.series.name}`
-                        }`}
-                        src={PosterImage({
-                          path: item.movie_id ? item.movie.poster : item.series.poster,
-                          size: "sm",
-                        })}
-                        width="170px"
-                        height="240px"
-                        className="rounded"
-                      />
-                      <div>
-                        <span className="w-full text-xs truncate line-clamp-2">
-                          {item?.movie_id
-                            ? `${item.movie.title}`
-                            : `${item.season_number}x${item.episode_number} ${item.series.name}`}
-                        </span>
-                        <div className="text-xs opacity-50 line-clamp-1">{date}</div>
-                      </div>
-                    </a>
-                  </Link>
-                  <div className="pt-1 text-gray-500 transition-all duration-300 ease-in-out opacity-25 group-hover:opacity-100">
-                    {deleteEpisodeFromHistory.isLoading || deleteMovieFromHistory.isLoading ? (
-                      <ImSpinner2 className="w-6 h-6 animate-spin" />
-                    ) : (
-                      <button
-                        className="text-3xl transition-all duration-300 ease-in-out hover:text-red-700"
-                        onClick={(e) => {
-                          e.preventDefault();
-                          e.stopPropagation();
-                          handleDelete(item.id, item.movie_id ? "movie" : "episode");
-                        }}
-                      >
-                        <MdDelete className="text-2xl" />
-                      </button>
-                    )}
-                  </div>
-                </motion.div>
-              );
+    <motion.div
+      layout
+      initial={{ opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ y: -150, opacity: 0 }}
+      transition={{ type: "spring" }}
+      className="relative w-[170px] group"
+    >
+      <Link
+        href={
+          item?.movie_id
+            ? `/movies/${item.movie.id}`
+            : `/tv/${item.series_id}/season/${item.season_number}/episode/${item.episode_number}`
+        }
+      >
+        <a className="relative w-[170px] group">
+          <Image
+            alt={`Poster image for ${
+              item?.movie_id ? item.movie.title : `${item.season_number}x${item.episode_number} ${item.series.name}`
+            }`}
+            src={PosterImage({
+              path: item.movie_id ? item.movie.poster : item.series.poster,
+              size: "sm",
             })}
-          </AnimatePresence>
-        </PosterGrid>
-      )}
-    </LoadingPageComponents>
+            width="170px"
+            height="240px"
+            className="rounded"
+          />
+          <div>
+            <span className="w-full text-xs truncate line-clamp-2">
+              {item?.movie_id
+                ? `${item.movie.title}`
+                : `${item.season_number}x${item.episode_number} ${item.series.name}`}
+            </span>
+            <div className="text-xs opacity-50 line-clamp-1">{date}</div>
+          </div>
+        </a>
+      </Link>
+      <div className="pt-1 text-gray-500 transition-all duration-300 ease-in-out opacity-25 group-hover:opacity-100">
+        {deleteEpisodeFromHistory.isLoading || deleteMovieFromHistory.isLoading ? (
+          <ImSpinner2 className="w-6 h-6 animate-spin" />
+        ) : (
+          <button
+            className="text-3xl transition-all duration-300 ease-in-out hover:text-red-700"
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              handleDelete(item.id, item.movie_id ? "movie" : "episode");
+            }}
+          >
+            <MdDelete className="text-2xl" />
+          </button>
+        )}
+      </div>
+    </motion.div>
   );
 };
 
