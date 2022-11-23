55ac51525c09336cbc311a7fd5b9095f4042b2d1
Rowan-Paul <contact@rowanpaulflynn.com>
2022-11-23 21:14:52 +0100

    Update stroke-linecap to strokeLinecap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,7 +50,7 @@ const Home: NextPage = () => {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
+                    strokeLinecap="round"
                     stroke-linejoin="round"
                     stroke-width="1.5"
                     d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
