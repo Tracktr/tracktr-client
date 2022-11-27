18b44e06e0e84ad4993a81ff26077605c378c257
Rowan-Paul <contact@rowanpaulflynn.com>
2022-11-27 14:08:38 +0100

    Add frontend

--- src/pages/profile/import.tsx
+++ src/pages/profile/import.tsx
@@ -1,4 +1,7 @@
+import { useRouter } from "next/router";
 import Papa from "papaparse";
+import { useState } from "react";
+import { ImSpinner2 } from "react-icons/im";
 import { trpc } from "../../utils/trpc";
 
 export interface ITraktData {
@@ -28,7 +31,11 @@ export interface ITraktData {
 }
 
 const ImportPage = () => {
-  const importRoute = trpc.profile.import.useMutation({});
+  const router = useRouter();
+
+  const importRoute = trpc.profile.import.useMutation({
+    onSuccess: () => router.push("/profile/history"),
+  });
 
   const changeHandler = (event: any) => {
     Papa.parse(event.target.files[0], {
@@ -69,13 +76,55 @@ const ImportPage = () => {
 
   return (
     <div className="max-w-6xl pt-24 m-auto">
-      <input
-        type="file"
-        name="file"
-        accept=".csv"
-        onChange={changeHandler}
-        style={{ display: "block", margin: "10px auto" }}
-      />
+      <div className="my-5">
+        <h1 className="mb-2 text-3xl">Import history from Trakt.tv</h1>
+        <div>Export your history from Trakt.tv by going to the history page and pressing the CSV button.</div>
+      </div>
+      <div className="flex items-center justify-center w-full">
+        <label
+          htmlFor="dropzone-file"
+          className={`flex flex-col items-center justify-center w-full h-64 bg-gray-700 border-2 border-gray-600 border-dashed rounded-lg ${
+            !importRoute.isLoading && "cursor-pointer hover:bg-bray-800 hover:border-gray-500 hover:bg-gray-600"
+          }`}
+        >
+          {importRoute.isLoading ? (
+            <div className="flex flex-col items-center">
+              <ImSpinner2 className="w-10 h-10 mb-4 animate-spin" />
+              <div>This might take a few minutes depending on the size of the file.</div>
+            </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center pt-5 pb-6">
+              <svg
+                aria-hidden="true"
+                className="w-10 h-10 mb-3 text-gray-400"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                ></path>
+              </svg>
+              <p className="mb-2 text-sm text-gray-400">
+                <span className="font-semibold">Click to upload</span> or drag and drop
+              </p>
+              <p className="text-xs text-gray-400">CSV only</p>
+            </div>
+          )}
+          <input
+            id="dropzone-file"
+            type="file"
+            accept=".csv"
+            disabled={importRoute.isLoading}
+            onChange={changeHandler}
+            className="hidden"
+          />
+        </label>
+      </div>
     </div>
   );
 };
