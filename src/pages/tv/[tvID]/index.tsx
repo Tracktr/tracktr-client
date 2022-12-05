e1570b075f9d4058ab5c6a39cf92954a89863f7d
Rowan-Paul <contact@rowanpaulflynn.com>
2022-12-05 10:14:58 +0100

    Remove comments

--- src/pages/tv/[tvID]/index.tsx
+++ src/pages/tv/[tvID]/index.tsx
@@ -35,30 +35,6 @@ const TVPage = () => {
   return (
     <LoadingPageComponents status={status}>
       {() => (
-        //   <ContentHeader
-        //     cover={data.backdrop_path}
-        //     poster={data.poster_path}
-        //     title={data.name}
-        //     description={data.overview}
-        //     genres={data.genres}
-        //     score={data.vote_average}
-        //     justWatch={data["watch/providers"]}
-        //     seriesProgression={data.number_of_episodes_watched}
-        //     amountOfEpisodes={data.number_of_episodes}
-        //     videos={data.videos}
-        //     themeColor={data.theme_color}
-        //     seriesID={data.id}
-        //   >
-        //     <DetailsBlock
-        //       status={data.status}
-        //       numberOfEpisodes={data.number_of_episodes}
-        //       numberOfSeasons={data.number_of_seasons}
-        //     />
-        //     <SeasonsBlock seasons={data.seasons} />
-        //     <CastBlock cast={data.credits.cast} />
-        //     <CrewBlock crew={data.credits.crew} />
-        //   </ContentHeader>
-        // )}
         <>
           <div
             className="absolute w-screen max-w-full h-64 md:h-[32rem] top-0 left-0"
