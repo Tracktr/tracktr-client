f87c52beb062c9da00aecae70b40deff86d23cfd
Rowan-Paul <contact@rowanpaulflynn.com>
2022-12-05 14:07:19 +0100

    Add review modal

--- src/components/common/ReviewButton.tsx
+++ src/components/common/ReviewButton.tsx
@@ -1,11 +1,22 @@
+import { ChangeEvent, useState } from "react";
 import { MdReviews } from "react-icons/md";
 import ReactTooltip from "react-tooltip";
+import Modal from "../modal/Modal";
 import { IThemeColor } from "../watchButton/BaseWatchButton";
 
 const ReviewButton = ({ themeColor }: { themeColor: IThemeColor }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleInput = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = e.currentTarget;
+    setSearchInput(value);
+  };
+
   return (
     <div>
       <button
+        onClick={() => setModalOpen(!modalOpen)}
         style={{
           backgroundColor: themeColor.hex,
         }}
@@ -20,6 +31,34 @@ const ReviewButton = ({ themeColor }: { themeColor: IThemeColor }) => {
           <MdReviews className="text-2xl" />
         </span>
       </button>
+      {modalOpen && (
+        <Modal handleClose={() => setModalOpen(!modalOpen)}>
+          <div className="py-4">
+            <div className="text-2xl">Write a review</div>
+            <div className="mt-2">
+              <textarea
+                id="about"
+                name="about"
+                rows={3}
+                className="block w-full p-2 mt-1 bg-gray-700 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                value={searchInput}
+                onChange={handleInput}
+              />
+              <p className="mt-2 text-sm text-gray-500">Max 512 characters</p>
+              <button
+                style={{
+                  backgroundColor: themeColor.hex,
+                }}
+                className={`flex items-center justify-between px-3 py-2 mt-3 rounded-md ${
+                  themeColor.isDark && "text-white"
+                } ${themeColor.isLight && "text-primaryBackground"}`}
+              >
+                <div>Submit</div>
+              </button>
+            </div>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
