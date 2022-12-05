3c6672c487f81ea801e9ce181d78d36640a0b012
Rowan-Paul <contact@rowanpaulflynn.com>
2022-12-05 15:32:25 +0100

    Add link to profile on comment

--- src/components/pageBlocks/ReviewsBlock.tsx
+++ src/components/pageBlocks/ReviewsBlock.tsx
@@ -1,34 +1,41 @@
+import Link from "next/link";
 import ImageWithFallback from "../common/ImageWithFallback";
 
 const ReviewsBlock = ({ reviews }: IReviewsBlock) => {
-  console.log(reviews);
-
   return (
     <div className="relative mx-1 md:mx-0 md:mb-8">
       <h2 className="pb-4 text-4xl font-bold">Reviews</h2>
       <div className="flex flex-col gap-6">
-        {reviews.map((review) => (
-          <div key={review.id}>
-            <div className="flex items-center gap-2 mb-2">
-              <ImageWithFallback
-                src={review.user.image}
-                fallbackSrc="/placeholder_profile.png"
-                width="32"
-                height="32"
-                alt="Profile picture"
-                className="rounded-full"
-              />
-              <p className="text-xl">{review.user.profile.username}</p>
-              <div className="ml-auto text-sm">
-                {review.created.toLocaleString("en-UK", {
-                  dateStyle: "medium",
-                  timeStyle: "short",
-                })}
+        {reviews.length > 0 ? (
+          reviews.map((review) => (
+            <div key={review.id}>
+              <div className="flex items-center gap-2 mb-2">
+                <Link href={`/profile/${review.user.profile.username}`}>
+                  <a className="flex items-center gap-2">
+                    <ImageWithFallback
+                      src={review.user.image}
+                      fallbackSrc="/placeholder_profile.png"
+                      width="32"
+                      height="32"
+                      alt="Profile picture"
+                      className="rounded-full"
+                    />
+                    <p className="text-xl">{review.user.profile.username}</p>
+                  </a>
+                </Link>
+                <div className="ml-auto text-sm">
+                  {review.created.toLocaleString("en-UK", {
+                    dateStyle: "medium",
+                    timeStyle: "short",
+                  })}
+                </div>
               </div>
+              <div>{review.content}</div>
             </div>
-            <div>{review.content}</div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <div>No reviews found</div>
+        )}
       </div>
     </div>
   );
