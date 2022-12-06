1d0ab4a93e9d9dfe67ce5917fd552bfb7134921d
Patrick Roelofs <contact@patrickroelofs.com>
2022-12-06 13:08:30 +0100

    groupBy function

--- src/components/pageBlocks/CreditsBlock.tsx
+++ src/components/pageBlocks/CreditsBlock.tsx
@@ -9,7 +9,30 @@ interface CreditsBlockProps {
   credits: any;
 }
 
+function groupBy<T extends Record<string, any>, K extends keyof T>(
+  array: T[],
+  key: K | { (obj: T): string }
+): Record<string, T[]> {
+  const keyFn = key instanceof Function ? key : (obj: T) => obj[key];
+  return array.reduce((objectsByKeyValue, obj) => {
+    const value = keyFn(obj);
+    objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
+    return objectsByKeyValue;
+  }, {} as Record<string, T[]>);
+}
+
 const CreditsBlock = ({ credits }: CreditsBlockProps) => {
+  const movieCreditsByJob = Object.entries(groupBy(credits.movie.crew, "job")).map(([title, data]) => ({
+    title,
+    data,
+  }));
+  const tvCreditsByJob = Object.entries(groupBy(credits.tv.crew, "job")).map(([title, data]) => ({
+    title,
+    data,
+  }));
+
+  console.log(movieCreditsByJob, tvCreditsByJob);
+
   return (
     <div>
       <h2 className="pb-4 text-3xl font-bold">Known for</h2>
