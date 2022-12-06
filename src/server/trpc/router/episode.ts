6df7028d8799c8058713be10d21c56a88e87ca41
Rowan-Paul <contact@rowanpaulflynn.com>
2022-12-06 11:45:52 +0100

    Add season & episode review to episode by id route

--- src/server/trpc/router/episode.ts
+++ src/server/trpc/router/episode.ts
@@ -23,8 +23,45 @@ export const episodeRouter = router({
       const res = await fetch(url);
       const json = await res.json();
 
+      const databaseEpisodes = await ctx.prisma.episodes.findFirst({
+        where: { episode_number: Number(input.episodeID), season_number: Number(input.episodeID) },
+        include: {
+          EpisodesReviews: {
+            include: {
+              user: {
+                include: {
+                  profile: true,
+                },
+              },
+            },
+            take: 10,
+            orderBy: {
+              created: "desc",
+            },
+          },
+        },
+      });
+
+      if (!databaseEpisodes) {
+        const showUrl = new URL(`tv/${input?.tvID}`, process.env.NEXT_PUBLIC_TMDB_API);
+        showUrl.searchParams.append("api_key", process.env.NEXT_PUBLIC_TMDB_KEY || "");
+
+        const show = await fetch(showUrl).then((res) => res.json());
+
+        const seriesPoster = show.poster_path ? show.poster_path : "/noimage.png";
+
+        const newSeries = await createNewSeries({ show, seriesPoster, id: Number(input.tvID) });
+
+        await ctx.prisma.series.upsert({
+          where: { id: Number(input.tvID) },
+          update: newSeries,
+          create: newSeries,
+        });
+      }
+
       return {
         ...json,
+        reviews: databaseEpisodes?.EpisodesReviews || [],
       };
     }),
 
