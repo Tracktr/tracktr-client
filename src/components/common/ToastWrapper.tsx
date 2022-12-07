9e64f0e4542cfe259037d613b303cce5e479a40a
Patrick Roelofs <contact@patrickroelofs.com>
2022-12-07 13:50:04 +0100

    update styling and add button to places

--- src/components/common/ToastWrapper.tsx
+++ src/components/common/ToastWrapper.tsx
@@ -3,8 +3,8 @@ import { ToastContainer } from "react-toastify";
 const ToastWrapper = () => {
   return (
     <ToastContainer
-      toastClassName={"relative p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer"}
-      bodyClassName={() => "text-md font-medium p-3 text-black flex items-center"}
+      toastClassName={"relative p-1 rounded-md justify-between overflow-hidden cursor-pointer"}
+      bodyClassName={() => "text-xs font-medium text-black flex items-center"}
       position="bottom-right"
       autoClose={1500}
       icon={false}
