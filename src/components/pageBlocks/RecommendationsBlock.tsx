89237499f5a626e064f13a854d756c28a78ab344
Patrick Roelofs <contact@patrickroelofs.com>
2022-12-08 14:01:20 +0100

    add title

--- src/components/pageBlocks/RecommendationsBlock.tsx
+++ src/components/pageBlocks/RecommendationsBlock.tsx
@@ -10,32 +10,35 @@ const RecommendationsBlock = ({ recommendations, type }: RecommendationsProps) =
   console.log(recommendations);
 
   return (
-    <div className="grid w-full grid-cols-6 mt-32">
-      {recommendations.results.length > 0 &&
-        recommendations.results.slice(0, 6).map((recommendation: any) => {
-          const image = BackdropImage({
-            path: recommendation.backdrop_path,
-            size: "lg",
-          });
+    <div className="mt-32">
+      <p className="px-4 pb-1 text-3xl italic font-normal opacity-20">Recommended {type}</p>
+      <div className="grid w-full grid-cols-6">
+        {recommendations.results.length > 0 &&
+          recommendations.results.slice(0, 6).map((recommendation: any) => {
+            const image = BackdropImage({
+              path: recommendation.backdrop_path,
+              size: "lg",
+            });
 
-          return (
-            <Link href={`/${type}/${recommendation.id}`} key={recommendation.id}>
-              <a>
-                <div
-                  key={recommendation.id}
-                  className="relative w-full h-52"
-                  style={{
-                    backgroundImage: `url(${image})`,
-                    backgroundPosition: "center center",
-                    backgroundSize: "cover",
-                  }}
-                >
-                  <p>{recommendation.title}</p>
-                </div>
-              </a>
-            </Link>
-          );
-        })}
+            return (
+              <Link href={`/${type}/${recommendation.id}`} key={recommendation.id}>
+                <a>
+                  <div
+                    key={recommendation.id}
+                    className="relative w-full h-52"
+                    style={{
+                      backgroundImage: `url(${image})`,
+                      backgroundPosition: "center center",
+                      backgroundSize: "cover",
+                    }}
+                  >
+                    <p>{recommendation.title}</p>
+                  </div>
+                </a>
+              </Link>
+            );
+          })}
+      </div>
     </div>
   );
 };
