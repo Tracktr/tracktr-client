e5f097254b39edd43cdaf1478595ef761d61c0db
Patrick Roelofs <contact@patrickroelofs.com>
2022-12-08 14:44:06 +0100

    remove recommendations when there is no backdrop

--- src/components/pageBlocks/RecommendationsBlock.tsx
+++ src/components/pageBlocks/RecommendationsBlock.tsx
@@ -11,30 +11,33 @@ const RecommendationsBlock = ({ recommendations, type }: RecommendationsProps) =
 
   return (
     recommendations.results.length > 6 && (
-      <div className="mt-32">
+      <div className="hidden mt-32 lg:block">
         <p className="px-4 pb-1 text-3xl italic font-normal opacity-20">Recommended {type}</p>
         <div className="grid w-full grid-cols-6">
-          {recommendations.results.slice(0, 6).map((recommendation: any) => {
-            const image = BackdropImage({
-              path: recommendation.backdrop_path,
-              size: "lg",
-            });
+          {recommendations.results
+            .filter((element: { backdrop_path: null }) => element.backdrop_path !== null)
+            .slice(0, 6)
+            .map((recommendation: any) => {
+              const image = BackdropImage({
+                path: recommendation.backdrop_path,
+                size: "lg",
+              });
 
-            return (
-              <Link href={`/${type}/${recommendation.id}`} key={recommendation.id}>
-                <a
-                  className="relative flex items-end w-full h-52 group"
-                  style={{
-                    background: `linear-gradient(180deg, rgba(26,26,26,0) 0%, rgba(26,26,26,1) 100%), url(${image})`,
-                    backgroundPosition: "center center",
-                    backgroundSize: "cover",
-                  }}
-                >
-                  <p className="pb-4 mx-4 truncate overflow-ellipsis">{recommendation.title}</p>
-                </a>
-              </Link>
-            );
-          })}
+              return (
+                <Link href={`/${type}/${recommendation.id}`} key={recommendation.id}>
+                  <a
+                    className="relative flex items-end w-full h-52 group"
+                    style={{
+                      background: `linear-gradient(180deg, rgba(26,26,26,0) 0%, rgba(26,26,26,1) 100%), url(${image})`,
+                      backgroundPosition: "center center",
+                      backgroundSize: "cover",
+                    }}
+                  >
+                    <p className="pb-4 mx-4 truncate overflow-ellipsis">{recommendation.title}</p>
+                  </a>
+                </Link>
+              );
+            })}
         </div>
       </div>
     )
