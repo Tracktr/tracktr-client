409b0be121d16eda491be33b1b47246d99e75913
Rowan-Paul <contact@rowanpaulflynn.com>
2023-01-20 16:16:06 +0100

    Fix feedback page copy

--- src/pages/feedback.tsx
+++ src/pages/feedback.tsx
@@ -70,9 +70,9 @@ const FeedbackPage = () => {
             <div className="max-w-2xl mx-auto text-center">
               <h2 className="text-3xl font-bold leading-tight sm:text-4xl lg:text-5xl">Feedback form</h2>
               <p className="mx-auto mt-4 text-base leading-relaxed">
-                If you see something that isn&apos;t working quite right or you have an awesome idea htmlFor a new
-                feature you can leave a message on this page and we&apos;ll take a look at it. If you have a GitHub
-                account, you can also create an issue on{" "}
+                If you see something that isn&apos;t working quite right or you have an awesome idea for a new feature
+                you can leave a message on this page and we&apos;ll take a look at it. If you have a GitHub account, you
+                can also create an issue on{" "}
                 <a
                   className="underline"
                   href="https://github.com/Tracktr/tracktr-client/issues"
