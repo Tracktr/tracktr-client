a29c8fb884428106dcda84bc06059e3fd90a0b91
Rowan-Paul <contact@rowanpaulflynn.com>
2023-01-26 13:30:14 +0100

    Fix no reviews on dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -171,6 +171,8 @@ const DashboardPage = () => {
                 />
 
                 <h2 className="my-2 text-xl">Reviews</h2>
+
+                {(friendsData?.seriesReviews || friendsData?.movieReviews) && <div>No reviews found</div>}
                 <div className="flex flex-col md:flex-row">
                   {friendsStatus === "loading" ? (
                     <LoadingFriendReview />
