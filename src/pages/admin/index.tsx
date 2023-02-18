a98b70a51baaa84f111e4b33d2f1b9daccc91f20
Rowan-Paul <contact@rowanpaulflynn.com>
2023-02-18 18:12:43 +0100

    Add approve review component

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,17 +1,15 @@
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { ImSpinner2 } from "react-icons/im";
+import { useEffect } from "react";
 import { IoIosAdd, IoIosRemove, IoMdInformation } from "react-icons/io";
 import { MdDelete } from "react-icons/md";
 import { toast } from "react-toastify";
+import ApproveReview from "../../components/admin/ApproveReview";
 import LoadingPageComponents from "../../components/common/LoadingPageComponents";
-import Review from "../../components/common/Review";
 import { trpc } from "../../utils/trpc";
 
 const AdminPage = () => {
-  const [currentLoadingID, setCurrentLoadingID] = useState("");
   const { status: sessionStatus, data: sessionData } = useSession();
   const router = useRouter();
 
@@ -210,257 +208,33 @@ const AdminPage = () => {
             </div>
 
             <div className="flex flex-col gap-4 mt-4 md:flex-row">
-              <div className="md:w-[50%] bg-[#1A1A1A] p-4 rounded">
-                <div className="text-xl font-bold">Movie reviews</div>
-                {reviews && reviews.movies && reviews.movies.length > 0 ? (
-                  <div className="flex flex-col gap-6">
-                    {reviews.movies.map((review) => (
-                      <div key={review.id}>
-                        <Review
-                          content={review.content}
-                          created={review.created}
-                          friend={{
-                            image: review.user.image,
-                            name: review.user.profile?.username,
-                          }}
-                          item={review.Movies}
-                          hideImage
-                        />
-                        <button
-                          className="w-full px-2 py-1 mb-2 mr-2 text-sm font-medium text-center text-white bg-blue-600 rounded-lg focus:ring-4 focus:outline-none sm:w-auto hover:bg-blue-700 focus:ring-blue-800 disabled:cursor-not-allowed disabled:bg-gray-700"
-                          onClick={() => {
-                            setCurrentLoadingID(review.id);
-                            approveMoviesReview.mutate({
-                              reviewID: review.id,
-                            });
-                          }}
-                          disabled={approveMoviesReview.isLoading && currentLoadingID === review.id}
-                        >
-                          {approveMoviesReview.isLoading && currentLoadingID === review.id ? (
-                            <div className="flex items-center gap-2">
-                              <ImSpinner2 className="animate-spin" />
-                            </div>
-                          ) : (
-                            <div>Approve</div>
-                          )}
-                        </button>
-                        <button
-                          className="w-full px-2 py-1 text-sm font-medium text-center text-white bg-red-600 rounded-lg focus:ring-4 focus:outline-none sm:w-auto hover:bg-red-700 focus:red-blue-800 disabled:cursor-not-allowed disabled:bg-gray-700"
-                          onClick={() => {
-                            setCurrentLoadingID(review.id);
-                            removeMoviesReview.mutate({
-                              reviewID: review.id,
-                            });
-                          }}
-                          disabled={removeMoviesReview.isLoading && currentLoadingID === review.id}
-                        >
-                          {removeMoviesReview.isLoading && currentLoadingID === review.id ? (
-                            <div className="flex items-center gap-2">
-                              <ImSpinner2 className="animate-spin" />
-                            </div>
-                          ) : (
-                            <div>Remove</div>
-                          )}
-                        </button>
-                      </div>
-                    ))}
-                  </div>
-                ) : (
-                  <div>No (unapproved) movie reviews</div>
-                )}
-              </div>
-
-              <div className="md:w-[50%] bg-[#1A1A1A] p-4 rounded">
-                <div className="text-xl font-bold">Series reviews</div>
-                {reviews && reviews.series && reviews.series.length > 0 ? (
-                  <div className="flex flex-col gap-6">
-                    {reviews.series.map((review) => (
-                      <div key={review.id}>
-                        <Review
-                          content={review.content}
-                          created={review.created}
-                          friend={{
-                            image: review.user.image,
-                            name: review.user.profile?.username,
-                          }}
-                          item={review.Series}
-                          hideImage
-                        />
-
-                        <button
-                          className="w-full px-2 py-1 mb-2 mr-2 text-sm font-medium text-center text-white bg-blue-600 rounded-lg focus:ring-4 focus:outline-none sm:w-auto hover:bg-blue-700 focus:ring-blue-800 disabled:cursor-not-allowed disabled:bg-gray-700"
-                          onClick={() => {
-                            setCurrentLoadingID(review.id);
-                            approveSeriesReview.mutate({
-                              reviewID: review.id,
-                            });
-                          }}
-                          disabled={approveSeriesReview.isLoading && currentLoadingID === review.id}
-                        >
-                          {approveSeriesReview.isLoading && currentLoadingID === review.id ? (
-                            <div className="flex items-center gap-2">
-                              <ImSpinner2 className="animate-spin" />
-                            </div>
-                          ) : (
-                            <div>Approve</div>
-                          )}
-                        </button>
-                        <button
-                          className="w-full px-2 py-1 text-sm font-medium text-center text-white bg-red-600 rounded-lg focus:ring-4 focus:outline-none sm:w-auto hover:bg-red-700 focus:red-blue-800 disabled:cursor-not-allowed disabled:bg-gray-700"
-                          onClick={() => {
-                            setCurrentLoadingID(review.id);
-                            removeSeriesReview.mutate({
-                              reviewID: review.id,
-                            });
-                          }}
-                          disabled={removeSeriesReview.isLoading && currentLoadingID === review.id}
-                        >
-                          {removeSeriesReview.isLoading && currentLoadingID === review.id ? (
-                            <div className="flex items-center gap-2">
-                              <ImSpinner2 className="animate-spin" />
-                            </div>
-                          ) : (
-                            <div>Remove</div>
-                          )}
-                        </button>
-                      </div>
-                    ))}
-                  </div>
-                ) : (
-                  <div>No (unapproved) series reviews</div>
-                )}
-              </div>
+              <ApproveReview
+                approveItem={approveMoviesReview}
+                removeItem={removeMoviesReview}
+                type="movie"
+                reviews={reviews?.movies}
+              />
+              <ApproveReview
+                approveItem={approveSeriesReview}
+                removeItem={removeSeriesReview}
+                type="series"
+                reviews={reviews?.series}
+              />
             </div>
 
             <div className="flex flex-col gap-4 mt-4 md:flex-row">
-              <div className="md:w-[50%] bg-[#1A1A1A] p-4 rounded">
-                <div className="text-xl font-bold">Season reviews</div>
-                {reviews && reviews.seasons && reviews.seasons.length > 0 ? (
-                  <div className="flex flex-col gap-6">
-                    {reviews.seasons.map((review) => (
-                      <div key={review.id}>
-                        <Review
-                          content={review.content}
-                          created={review.created}
-                          friend={{
-                            image: review.user.image,
-                            name: review.user.profile?.username,
-                          }}
-                          item={{
-                            id: Number(review.Seasons.Series?.id),
-                            name: `${review.Seasons.Series?.name} Season ${review.Seasons.season_number}`,
-                            poster: String(review.Seasons.Series?.poster),
-                          }}
-                          hideImage
-                        />
-                        <button
-                          className="w-full px-2 py-1 mb-2 mr-2 text-sm font-medium text-center text-white bg-blue-600 rounded-lg focus:ring-4 focus:outline-none sm:w-auto hover:bg-blue-700 focus:ring-blue-800 disabled:cursor-not-allowed disabled:bg-gray-700"
-                          onClick={() => {
-                            setCurrentLoadingID(review.id);
-                            approveSeasonsReview.mutate({
-                              reviewID: review.id,
-                            });
-                          }}
-                          disabled={approveSeasonsReview.isLoading && currentLoadingID === review.id}
-                        >
-                          {approveSeasonsReview.isLoading && currentLoadingID === review.id ? (
-                            <div className="flex items-center gap-2">
-                              <ImSpinner2 className="animate-spin" />
-                            </div>
-                          ) : (
-                            <div>Approve</div>
-                          )}
-                        </button>
-                        <button
-                          className="w-full px-2 py-1 text-sm font-medium text-center text-white bg-red-600 rounded-lg focus:ring-4 focus:outline-none sm:w-auto hover:bg-red-700 focus:red-blue-800 disabled:cursor-not-allowed disabled:bg-gray-700"
-                          onClick={() => {
-                            setCurrentLoadingID(review.id);
-                            removeSeasonsReview.mutate({
-                              reviewID: review.id,
-                            });
-                          }}
-                          disabled={removeSeasonsReview.isLoading && currentLoadingID === review.id}
-                        >
-                          {removeSeasonsReview.isLoading && currentLoadingID === review.id ? (
-                            <div className="flex items-center gap-2">
-                              <ImSpinner2 className="animate-spin" />
-                            </div>
-                          ) : (
-                            <div>Remove</div>
-                          )}
-                        </button>
-                      </div>
-                    ))}
-                  </div>
-                ) : (
-                  <div>No (unapproved) season reviews</div>
-                )}
-              </div>
-
-              <div className="md:w-[50%] bg-[#1A1A1A] p-4 rounded">
-                <div className="text-xl font-bold">Episodes reviews</div>
-                {reviews && reviews.episodes && reviews.episodes.length > 0 ? (
-                  <div className="flex flex-col gap-6">
-                    {reviews.episodes.map((review) => (
-                      <div key={review.id}>
-                        <Review
-                          content={review.content}
-                          created={review.created}
-                          friend={{
-                            image: review.user.image,
-                            name: review.user.profile?.username,
-                          }}
-                          item={{
-                            id: Number(review.Episodes.Seasons?.Series?.id),
-                            name: `${review.Episodes.Seasons?.season_number}x${review.Episodes.episode_number} ${review.Episodes.Seasons?.Series?.name}`,
-                            poster: String(review.Episodes.Seasons?.Series?.poster),
-                          }}
-                          hideImage
-                        />
-
-                        <button
-                          className="w-full px-2 py-1 mb-2 mr-2 text-sm font-medium text-center text-white bg-blue-600 rounded-lg focus:ring-4 focus:outline-none sm:w-auto hover:bg-blue-700 focus:ring-blue-800 disabled:cursor-not-allowed disabled:bg-gray-700"
-                          onClick={() => {
-                            setCurrentLoadingID(review.id);
-                            approveEpisodesReview.mutate({
-                              reviewID: review.id,
-                            });
-                          }}
-                          disabled={approveEpisodesReview.isLoading && currentLoadingID === review.id}
-                        >
-                          {approveEpisodesReview.isLoading && currentLoadingID === review.id ? (
-                            <div className="flex items-center gap-2">
-                              <ImSpinner2 className="animate-spin" />
-                            </div>
-                          ) : (
-                            <div>Approve</div>
-                          )}
-                        </button>
-                        <button
-                          className="w-full px-2 py-1 text-sm font-medium text-center text-white bg-red-600 rounded-lg focus:ring-4 focus:outline-none sm:w-auto hover:bg-red-700 focus:red-blue-800 disabled:cursor-not-allowed disabled:bg-gray-700"
-                          onClick={() => {
-                            setCurrentLoadingID(review.id);
-                            removeEpisodesReview.mutate({
-                              reviewID: review.id,
-                            });
-                          }}
-                          disabled={removeEpisodesReview.isLoading && currentLoadingID === review.id}
-                        >
-                          {removeEpisodesReview.isLoading && currentLoadingID === review.id ? (
-                            <div className="flex items-center gap-2">
-                              <ImSpinner2 className="animate-spin" />
-                            </div>
-                          ) : (
-                            <div>Remove</div>
-                          )}
-                        </button>
-                      </div>
-                    ))}
-                  </div>
-                ) : (
-                  <div>No (unapproved) series reviews</div>
-                )}
-              </div>
+              <ApproveReview
+                approveItem={approveSeasonsReview}
+                removeItem={removeSeasonsReview}
+                type="season"
+                reviews={reviews?.seasons}
+              />
+              <ApproveReview
+                approveItem={approveEpisodesReview}
+                removeItem={removeEpisodesReview}
+                type="episodes"
+                reviews={reviews?.episodes}
+              />
             </div>
           </div>
         )}
