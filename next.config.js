8238df0fcb8d0d36a91675324b8a785f1f31fdf2
Rowan-Paul <contact@rowanpaulflynn.com>
2023-04-12 12:44:21 +0200

    Update packages

--- next.config.js
+++ next.config.js
@@ -6,10 +6,6 @@ const nextConfig = {
     domains: ["image.tmdb.org", "lh3.googleusercontent.com"],
     unoptimized: true,
   },
-  experimental: {
-    appDir: true,
-    typedRoutes: true,
-  },
 };
 
 module.exports = nextConfig;
